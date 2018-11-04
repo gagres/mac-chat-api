10842c94a5149fe9010216861fe8cd5d5d02d7c3
Gabriel Giro Resende <gabriel.giro.resende@outlook.com>
2018-11-04 04:15:01 -0200

    Added mongodb url

--- src/config/index.js
+++ src/config/index.js
@@ -4,6 +4,6 @@ export default {
   // "port": 3005,
   // "mongoUrl": "mongodb://localhost:27017/chat-api",
   "port": process.env.PORT,
-  "mongoUrl": process.env.MONGODB_URI,
+  "mongoUrl": "mongodb://gagres:Gagres1!@ds213239.mlab.com:13239/chattychatchat",
   "bodyLimit": "100kb"
 }
